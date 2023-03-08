1035b145797839a5ea8848f3aabed148de219910
unlimited <osx@pop-os.localdomain>
2023-03-08 13:30:09 -0800

    init

--- app/src/main/java/com/driuft/androidkotlindebugging/ui/NumSumActivity.kt
+++ app/src/main/java/com/driuft/androidkotlindebugging/ui/NumSumActivity.kt
@@ -14,7 +14,8 @@ class NumSumActivity : AppCompatActivity() {
         setContentView(R.layout.activity_num_sum)
 
         val a = 2
-        val b = "2" as Int
+        val b = "2".toInt()
+        // val b = 2
 
         resultText.text = "${sumOfTwo(a, b)}"
     }
